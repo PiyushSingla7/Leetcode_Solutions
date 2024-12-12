dc3d77d823d24650447948b440824602a026dca2
Piyush <122970842+PiyushSingla7@users.noreply.github.com>
2024-12-12 11:46:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,30 @@
 class Solution {
-    public int[] searchRange(int[] arr, int target) {
-        int[] ans = { -1, -1 };
-        int n = arr.length;
-        int low = 0, high = n - 1;
-        boolean flag = false;
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1, fp = -1;
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == target) {
-                flag = true;
-                break;
-            } else if (arr[mid] > target) {
+            int mid = (low) + (high - low) / 2;
+            if (nums[mid] == target) {
+                if (mid > 0 && nums[mid - 1] == target)
+                    high = mid - 1;
+                else {
+                    fp = mid;
+                    break;
+                }
+            } else if (nums[mid] > target)
                 high = mid - 1;
-            } else
+            else
                 low = mid + 1;
         }
-        if (flag) {
-            low = 0;
-            high = n - 1;
-            int lb = n;
-            while (low <= high) {
-                int mid = (low) + (high - low) / 2;
-                if (arr[mid] >= target) {
-                    lb = Math.min(lb, mid);
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            ans[0] = lb;
-            low = 0;
-            high = n - 1;
-            int ub = n;
-            while (low <= high) {
-                int mid = (low) + (high - low) / 2;
-                if (arr[mid] > target) {
-                    ub = Math.min(ub, mid);
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            ans[1] = ub - 1;
+        int lp = fp;
+        int[] ans = { fp, lp };
+        if (lp == -1) {
             return ans;
-        } else
-            return ans;
+        }
+        while (nums[lp] == target)
+            lp++;
+        ans[0] = fp;
+        ans[1] = lp - 1;
+        return ans;
     }
 }
