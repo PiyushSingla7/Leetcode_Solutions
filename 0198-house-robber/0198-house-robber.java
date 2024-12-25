730fb0064f0408f520e069b955f1ea19a942ecb2
Piyush <122970842+PiyushSingla7@users.noreply.github.com>
2024-12-25 17:58:31 +0530

    Time: 0 ms (100%), Space: 41.7 MB (5.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -15,5 +15,17 @@ class Solution {
         dp = new int[nums.length];
         Arrays.fill(dp, -1);
         return amount(nums, 0, dp);
+
+        // tabulation way
+        // int n = arr.length;
+        // if (n == 1)
+        //     return nums[0];
+        // int[] dp = new int[n];
+        // dp[0] = nums[0];
+        // dp[1] = Math.max(nums[0], nums[1]);
+        // for (int i = 2; i < n; i++) {
+        //     dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        // }
+        // return dp[n - 1];
     }
 }
