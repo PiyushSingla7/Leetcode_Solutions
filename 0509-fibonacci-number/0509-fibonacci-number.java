4f817d585822f3591e7dd661738e458d2ed56b3f
Piyush <122970842+PiyushSingla7@users.noreply.github.com>
2025-01-16 23:39:50 +0530

    Time: 0 ms (100%), Space: 40.3 MB (75.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,20 +1,12 @@
 class Solution {
-    static int[] dp;
-
-    public int fibo(int n) {
-        if (n <= 1)
-            return n;
-        if (dp[n] != 0)
-            return dp[n];
-        else {
-            int ans = fibo(n - 1) + fibo(n - 2);
-            dp[n] = ans;
-            return ans;
-        }
-    }
-
     public int fib(int n) {
-        dp = new int[n + 1];
-        return fibo(n);
+        if(n<2) return n;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
     }
 }
