7bafb100e90991767cdd16cc62e9f52f0d414217
Piyush <122970842+PiyushSingla7@users.noreply.github.com>
2025-01-28 02:06:59 -0800

    Time: 11 ms (91.29%), Space: 49.1 MB (25.45%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int ele : nums) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        for (int ele : map.keySet()) {
+            int val = map.get(ele);
+            if (bucket[val] == null) {
+                bucket[val] = new ArrayList<>();
+            }
+            bucket[val].add(ele);
+        }
+        int[] ans = new int[k];
+        int idx = 0;
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int j = 0; j < bucket[i].size() && idx < k; j++) {
+                    ans[idx] = bucket[i].get(j);
+                    idx++;
+                }
+            }
+        }
+        return ans;
+    }
+}
