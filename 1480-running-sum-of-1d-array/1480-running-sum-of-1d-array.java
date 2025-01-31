40205455c8808984a9b8878577673cf17409c817
Piyush <122970842+PiyushSingla7@users.noreply.github.com>
2025-01-31 06:58:57 -0800

    Time: 0 ms (100%), Space: 42.9 MB (24.65%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-        result[0] = nums[0];
+        int[] prefixsum = new int[nums.length];
+        prefixsum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i - 1] + nums[i];
+            prefixsum[i] = nums[i] + prefixsum[i - 1];
         }
-        return result;
+        return prefixsum;
     }
 }
